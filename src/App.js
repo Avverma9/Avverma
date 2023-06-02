7b712489f9838bcc025a8c0a37c76530c322397e
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-06-03 00:24:24 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Component/Header/Header';
+// import Home from './Component/Home/Home';
+// import About from './Component/About/About';
+// import Contact from './Component/Contact/Contact';
 import Register from './Component/Register/Register.jsx';
 import Login from './Component/Login/Login';
 import Profile from './Component/Profile/Profile.jsx';
 import HeaderImage from './Component/HeaderImage/HeaderImage';
+
+// import CarouselPage from './Component/Carousel/Carousel';
+// import IndividualIntervalsExample from './Component/Carousel/Carousel';
 import Carousel from './Component/Carousel/Carousel';
 import SearchComponent from './Component/Search/Search';
 import Home from './Component/Home/Home.jsx'
+
 import ChatBox from './Component/Chatbot/Chatbot';
 import PopupCard from './Component/Welcome/Welcome';
 import Footer from './Component/Footer/Footer';
 import Hotel from './Component/Hotel/Hotel.jsx';
+
 import UserSettings from './Component/Profile/UserAction';
 import Partner from './Component/Partner/Partner';
-
 
 
 
@@ -24,8 +31,7 @@ function App() {
     <Router>
       <div>
         <Header />
-
-
+       
          <HeaderImage/>
        
    <Carousel/>
--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import './Login.css';
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import "./Login.css";
+import Google from "../SingGoogle/Google.jsx";
 
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [isLoading, setIsLoading] = useState(false); // Add loading state
 
@@ -15,26 +16,29 @@ const Login = () => {
     setIsLoading(true); // Set loading state to true while making the request
 
     try {
-      const response = await fetch('https://hotel-backend-tge7.onrender.com/signin', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, password })
-      });
+      const response = await fetch(
+        "https://hotel-backend-tge7.onrender.com/signin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       if (response.ok) {
         const data = await response.json();
         const { userId } = data;
 
-        localStorage.setItem('isSignedIn', 'true');
-        localStorage.setItem('userId', userId);
-        navigate('/'); // Redirect to the profile page
+        localStorage.setItem("isSignedIn", "true");
+        localStorage.setItem("userId", userId);
+        navigate("/"); // Redirect to the profile page
       } else {
-        console.log('Sign in failed');
+        console.log("Sign in failed");
       }
     } catch (error) {
-      console.log('Error:', error);
+      console.log("Error:", error);
     } finally {
       setIsLoading(false); // Set loading state to false after the request is complete
     }
@@ -45,16 +49,15 @@ const Login = () => {
   };
 
   const handleSignUp = () => {
-    navigate('/register'); // Redirect to the signup page
+    navigate("/register"); // Redirect to the signup page
   };
 
-  if (location.pathname !== '/signin') {
+  if (location.pathname !== "/signin") {
     return null;
   }
 
   return (
     <div className="card-signin">
-     
       {isLoading && (
         <img
           src="https://i.pinimg.com/originals/65/ba/48/65ba488626025cff82f091336fbf94bb.gif"
@@ -65,23 +68,22 @@ const Login = () => {
 
       <form onSubmit={handleSignIn}>
         <div className="form-group-signin">
-          
           <input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            placeholder='Enter Your Username'
+            placeholder="Enter Your Username"
             className="input-signin"
           />
         </div>
         <div className="form-group-signin">
           <input
-            type={showPassword ? 'text' : 'password'}
+            type={showPassword ? "text" : "password"}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            placeholder='Enter Your Password'
+            placeholder="Enter Your Password"
             className="input-signin"
           />
           <button
@@ -89,18 +91,25 @@ const Login = () => {
             className="show-password-button-signin"
             onClick={togglePasswordVisibility}
           >
-            {showPassword ? 'Hide' : 'Show'}
+            {showPassword ? "Hide" : "Show"}
           </button>
         </div>
         <button type="submit-login" disabled={isLoading}>
           Sign In
         </button>
+        <Google />
         <br />
-        <div className='dhac'>
-        Don't have an account?
-        <button type="button" onClick={handleSignUp} className="signup-button">
-          Click Here
-        </button></div>
+
+        <div className="dhac">
+          Don't have an account?
+          <button
+            type="button"
+            // onClick={handleSignUp}
+            className="signup-button"
+          >
+            Click Here
+          </button>
+        </div>
       </form>
       <br />
     </div>
