f0de009e66a8161d934e8207d68b0bf25ba0f15e
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-06-16 07:56:07 +0000

    updated

--- index.js
+++ index.js
@@ -593,13 +593,14 @@ app.get('/get/all/hotels', async (req, res) => {
 //=========================get hotels by state===============================//
 app.get('/get/all/hotels/:state', async (req, res) => {
   try {
-    const data= req.params.destination
-    const hotels = await Hotels.find((data));
+    const state = req.params.destination;
+    const hotels = await Hotels.find({ state });
     res.json(hotels);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 //================================get hotels by id==================
 app.get('/hotels/:id', async (req, res) => {
   try {
