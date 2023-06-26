b2feb817ff9483bc9791e10e1350dc72926da129
Sourav-source <st.sourav.xaverian@gmail.com>
2023-06-26 16:36:11 +0530

    push

--- src/Component/Hotel/Hotel.jsx
+++ src/Component/Hotel/Hotel.jsx
@@ -24,7 +24,6 @@ function HotelList() {
     // document.getElementsByClassName("label")[0].textContent = `₹${minValue}`
     // document.getElementsByClassName("label")[1].textContent = `₹${maxValue}`
 
-
     if (minValue > 400 || maxValue < 4000) {
       axios.get(`https://hotel-backend-tge7.onrender.com/hotels/price/get/by?minPrice=${minValue}&maxPrice=${maxValue}`)
         .then(data => {
