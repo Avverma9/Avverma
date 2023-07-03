95bfefcaf4fd78cbad4a4eda7609d9f510557f1a
Zishanmohamma <136809335+Zishanmohamma@users.noreply.github.com>
2023-07-03 18:28:49 +0530

    a

--- src/Pages/Auction/Auction.jsx
+++ src/Pages/Auction/Auction.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 export const Auction = () => {
   return (
       <div auction_wrapper>
-          <div className="auction_header">
-              <h1></h1>
-              <input type="button" value="" />
+          <div className="auction_type_header">
+              <h1>Live Auctions</h1>
+              <input type="button" value="View All" />
           </div>
+          
     </div>
   )
 }
