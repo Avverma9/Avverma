e68815aebefcd39f933ea525dcddb632e2b07455
Sourav-source <st.sourav.xaverian@gmail.com>
2023-07-04 19:50:39 +0530

    update PageContainer.css, SideNavBar.css, SideNavBar.jsx and src/assets/index.js

--- src/Component/SideNavBar/SideNavBar.jsx
+++ src/Component/SideNavBar/SideNavBar.jsx
@@ -1,40 +1,71 @@
 import React from "react";
-import { FaUserCircle } from "react-icons/fa";
+import {
+  auction,
+  bid,
+  liveauction,
+  upcomingauction,
+  profile,
+  win,
+  user,
+} from "../../assets";
 import "./SideNavBar.css";
 
 export const SideNavBar = () => {
-    const navLists = [
-        { name: "Profile", icon: <FaUserCircle />, path: "/" },
-        { name: "Live Auction", icon: <FaUserCircle />, path: "/live-auction" },
-        {
-            name: "Upcoming Auction",
-            icon: <FaUserCircle />,
-            path: "/upcoming-auction",
-        },
-        { name: "User", icon: <FaUserCircle />, path: "/user" },
-        { name: "Auction", icon: <FaUserCircle />, path: "/auction" },
-        { name: "Your Wins", icon: <FaUserCircle />, path: "/wins" },
-        { name: "Bidding Status", icon: <FaUserCircle />, path: "/bidding-status" },
-    ];
+  const navLists = [
+    {
+      name: "Profile",
+      icon: <img src={user} alt="icon" srcset="" />,
+      path: "/",
+    },
+    {
+      name: "Live Auction",
+      icon: <img src={liveauction} alt="icon" srcset="" />,
+      path: "/live-auction",
+    },
+    {
+      name: "Upcoming Auction",
+      icon: <img src={upcomingauction} alt="icon" srcset="" />,
+      path: "/upcoming-auction",
+    },
+    {
+      name: "User",
+      icon: <img src={profile} alt="icon" srcset="" />,
+      path: "/user",
+    },
+    {
+      name: "Auction",
+      icon: <img src={auction} alt="icon" srcset="" />,
+      path: "/auction",
+    },
+    {
+      name: "Your Wins",
+      icon: <img src={win} alt="icon" srcset="" />,
+      path: "/wins",
+    },
+    {
+      name: "Bidding Status",
+      icon: <img src={bid} alt="icon" srcset="" />,
+      path: "/bidding-status",
+    },
+  ];
 
-    return (
-        <section className="side_nav_container">
-            <div className="side_nav_header">
-                <h1>Dashboard</h1>
-            </div>
-            <div className="side_nav_body">
-                <ul>
-                    {navLists.map((item, index) => (
-                        <li key={index}>
-                            {item.icon}
-                            <p>
-                                {" "}
-                                <a href={`${item.path}`}>{item.name}</a>
-                            </p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section className="side_nav_container">
+      <div className="side_nav_header">
+        <h1>Dashboard</h1>
+      </div>
+      <div className="side_nav_body">
+        <ul>
+          {navLists.map((item, index) => (
+            <li key={index}>
+              {item.icon}
+              <p>
+                <a href={`${item.path}`}>{item.name}</a>
+              </p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
