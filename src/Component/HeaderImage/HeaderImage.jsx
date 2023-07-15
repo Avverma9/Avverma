6ed642f7505130a5f282bada87b93dafd53b9fe1
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-07-15 19:31:32 +0530

    updated

--- src/Component/HeaderImage/HeaderImage.jsx
+++ src/Component/HeaderImage/HeaderImage.jsx
@@ -3,6 +3,7 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
+import Slider from "react-slick";
 import "./HeaderImage.css";
 
 const HeaderImage = () => {
@@ -13,12 +14,41 @@ const HeaderImage = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
-  if (location.pathname !== "/") {
+  const isHomePage = location.pathname === "/" || "/home";
+  if (!isHomePage) {
     return null;
   }
 
+  let settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="header">
+    <Slider {...settings}>
       <div className="city">
         <div className="dropdown">
           <div className="top-offers" onClick={toggleDropdown}>
@@ -187,7 +217,7 @@ const HeaderImage = () => {
           <p className="city-name">Haryana</p>
         </div>
       </a>
-    </div>
+    </Slider>
   );
 };
 
