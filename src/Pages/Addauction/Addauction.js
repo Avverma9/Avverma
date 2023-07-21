698a040e6c41f203c3cef135fbf425e2f96ebea1
Zishanmohamma <136809335+Zishanmohamma@users.noreply.github.com>
2023-07-21 15:30:23 +0530

    new

--- src/Pages/Addauction/Addauction.js
+++ src/Pages/Addauction/Addauction.js
@@ -4,75 +4,92 @@ import "./Addauction.css";
 
 export const Addauction = () => {
   return (
-    <div className="filter_container">
-      <div className="filter_body">
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn1.acedms.com%2Fphotos%2Flisting%2F2020-09-08%2F48a83f44d5836cf7f3c0304fea2e06d0_extra_large.jpg&f=1&nofb=1&ipt=f3795f26093deccfcc8b544459e25c123d84e37f2966ec44373bfc303feb6b04&ipo=images"
-          alt="bid_item"
-          srcset=""
-        />
-      </div>
+    
+
       <div className="auction-detail">
-        <div className="auction-details-a">
-          <ul>
-            <li>Region</li>
-            <li>Category</li>
-            <li>product Name</li>
-            <li>seller Name</li>
-            <li>R.C</li>
-            <li>Reserve Price</li>
-            <li>Start Date</li>
-            <li>End Date</li>
-            <li>Parking Name</li>
-            <li>Year Of menufecture</li>
-            <li>Quatation Validity</li>
-            <li>Valid term</li>
-          </ul>
-          <ul className="filter-value">
-            <li>North 24 porgana</li>
-            <li>2nd Class</li>
-            <li>Tata Ace Bull</li>
-            <li>zishan mohammad</li>
-            <li>Yes/No</li>
-            <li>700rs</li>
-            <li>12June2023</li>
-            <li>20june2023</li>
-            <li>North 24</li>
-            <li>2021</li>
-            <li>2026</li>
-            <li></li>
-          </ul>
+        <div className="dropdown-flex">
+          <div className="region">
+            <select className="select-option" id="select-option">
+              <option value="">Region</option>
+              <option value="name">Region 1</option>
+              <option value="mobile">Region 2</option>
+              <option value="email">Region 3</option>
+            </select>
+          </div>
+          <div className="category">
+            <select className="select-option" id="select-option">
+              <option value="">Category</option>
+              <option value="name">Category 1</option>
+              <option value="mobile">Category 2</option>
+              <option value="email">Category 3</option>
+            </select>
+          </div>
+          <div className="seller">
+            <select className="select-option" id="select-option">
+              <option value="">Seller</option>
+              <option value="name">Seller 1</option>
+              <option value="mobile">Seller 2</option>
+              <option value="email">Seller 3</option>
+            </select>
+          </div>
         </div>
-        <div className="auction-details-b">
-          <ul>
-            <li>Aggrement No</li>
-            <li>Payment</li>
-            <li>Status</li>
-            <li>Registration NO</li>
-            <li>Start price</li>
-            <li>Start time</li>
-            <li>End time</li>
-            <li>Fuel</li>
-            <li>Parking Address</li>
-            <li>Payment Term</li>
-            <li>Auction Fees</li>
-            <li>Other Information</li>
-          </ul>
-          <ul className="filter-value">
-            <li>65825675895</li>
-            <li>Online</li>
-            <li>Pending</li>
-            <li>4525687552</li>
-            <li>700rs</li>
-            <li>12AM</li>
-            <li>2pm</li>
-            <li>Diesel</li>
-            <li>Napabelly barasat</li>
-            <li>Online</li>
-            <li>100rs</li>
-          </ul>
+        <div className="addauction-new">
+          <label htmlFor="nameofproduct">Name of the product
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Ragistration Number">Ragistration Number
+            <input type="number" placeholder="name"/>
+          </label>
+          <label htmlFor="Agrement Number">Agrement Number
+            <input type="number" placeholder="name"/>
+          </label>
+          <label htmlFor="RC">RC
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Start Price">Start Price
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Reserve Price">Reserve Price
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Start Time">Start Time
+            <input type="date" placeholder="name"/>
+          </label>
+          <label htmlFor="Start Date">Start Date
+            <input type="date" placeholder="name"/>
+          </label>
+          <label htmlFor="End Time">End Time
+            <input type="date" placeholder="name"/>
+          </label>
+          <label htmlFor="End Date">End Date
+            <input type="date" placeholder="name"/>
+          </label>
+          <label htmlFor="Fuel Type">Fuel Type
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Parking Name">Parking Name
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Parking Address">Parking Address
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Year Of Manufecture">Year Of Manufecture
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Payment Term">Payment Term
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Auction Fees">Auction Fees
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Other Information">Other Information
+            <input type="text" placeholder="name"/>
+          </label>
+          <label htmlFor="Add Photo/Video">Add Photo/Video
+            <input type="text" placeholder="Add photo/Video"/>
+          </label>
         </div>
       </div>
-    </div>
+    
   );
 };
