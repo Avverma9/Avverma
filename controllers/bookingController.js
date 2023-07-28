ac16ea48d29be93778e75e685ad7ad821f13e287
Abdul Rub <a.rub@cleverpush.com>
2023-07-28 18:48:40 +0530

    updatebooking-api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -164,11 +164,36 @@ const getCheckingBooking = async(req,res)=>{
   }
 }
 
+//========================================================================
+const updateBookingDates = async (req, res) => {
+  try {
+    const { bookingId } = req.params;
+    const { checkInDate, checkOutDate } = req.body;
+
+    const booking = await bookingModel.findOne({ bookingId });
+
+    if (!booking) {
+      return res.status(404).json({ success: false, message: 'Booking not found' });
+    }
+
+    booking.checkInDate = checkInDate;
+    booking.checkOutDate = checkOutDate;
+
+    await booking.save();
+
+    res.status(200).json({ success: true, data:booking });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
+
+
 
 module.exports={createBooking,
   getConfirmedBookings,
   getFailedBookings,
    cancelBooking,
     getCancelledBooking,
-    getCheckingBooking
+    getCheckingBooking,
+    updateBookingDates,
   }
