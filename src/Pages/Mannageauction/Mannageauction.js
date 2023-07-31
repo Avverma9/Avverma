4162175ad702da6d4fd068d30b718abbe06df2ce
Zishanmohamma <136809335+Zishanmohamma@users.noreply.github.com>
2023-07-31 19:58:19 +0530

    new

--- src/Pages/Mannageauction/Mannageauction.js
+++ src/Pages/Mannageauction/Mannageauction.js
@@ -1,5 +1,9 @@
 import React from "react";
 import DataTable from "react-data-table-component";
+import { MdOutlineRemoveRedEye, MdDeleteOutline } from "react-icons/md";
+import { BiEdit } from "react-icons/bi";
+
+import "./Mannageauction.css";
 
 const data = [
   {
@@ -169,34 +173,64 @@ export const MannageAuction = () => {
     {
       name: "Registration No",
       selector: (row) => row.registrationNo,
+      sortable: true,
     },
     {
       name: "Agreement No",
       selector: (row) => row.agreementNumber,
+      sortable: true,
     },
     {
       name: "Category",
       selector: (row) => row.category,
+      sortable: true,
     },
     {
       name: "Product Auction Name",
       selector: (row) => row.productAuctionName,
+      sortable: true,
     },
     {
       name: "Start Date",
       selector: (row) => row.startDate,
+      sortable: true,
     },
     {
       name: "End Date",
       selector: (row) => row.endDate,
+      sortable: true,
     },
     {
       name: "Total Bid",
       selector: (row) => row.totalBid,
+      sortable: true,
     },
     {
       name: "Status",
       selector: (row) => row.auctionStatus,
+      sortable: true,
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+        <div className="_edit">
+          <MdOutlineRemoveRedEye
+            size="18"
+            color="#1a2333"
+            onClick={() => window.alert(row.productAuctionName)}
+          />
+          <BiEdit
+            size="18"
+            color="#1b3ea9"
+            onClick={() => window.alert(row.productAuctionName)}
+          />
+          <MdDeleteOutline
+            size="18"
+            color="#ff0000"
+            onClick={() => window.alert(row.productAuctionName)}
+          />
+        </div>
+      ),
     },
   ];
   return (
@@ -209,6 +243,18 @@ export const MannageAuction = () => {
         fixedHeader
         fixedHeaderScrollHeight="75vh"
         selectableRows
+        subHeader
+        subHeaderAlign="left"
+        subHeaderComponent={
+          <div className="_mannage-auction-sub-header">
+            <input type="text" placeholder="Search here" />
+            <button>Filter</button>
+            <button>Add New Auction</button>
+            <button>Upload Bulk</button>
+            <button>Refresh</button>
+          </div>
+        }
+        actions={<button>Export</button>}
       />
     </>
   );
