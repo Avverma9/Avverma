878668b4babcdb03bfb2c1cf7c02423ce895350b
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-07 22:14:47 +0530

    done

--- src/Component/Search/Search.jsx
+++ src/Component/Search/Search.jsx
@@ -1,27 +1,25 @@
-/* eslint-disable no-unused-vars */
-
-import React, { useState } from 'react';
-import { useLocation, useNavigate, Routes, Route } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import './Search.css';
-import moment from 'moment';
-import { getSearchState } from '../redux/SearchSlice';
-import { useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./Search.css";
+import moment from "moment";
+import { getSearchState } from "../redux/SearchSlice";
+import { useDispatch } from "react-redux";
 
 const SearchComponent = () => {
   const navigate = useNavigate();
-  const location = useLocation();
   const dispatch = useDispatch();
   const [searchData, setSearchData] = useState({
-    destination: '',
-    startDate: null,
-    endDate: null,
-    guests: '1',
-    numRooms: '1',
+    destination: "",
+    startDate: new Date(),
+    endDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+    guests: "1",
+    numRooms: "1",
     localId: false,
-    maritalStatus: '',
-    moreOptions: '',
+    maritalStatus: "unmarried",
+    moreOptions: "",
+    
   });
   const [searchResults, setSearchResults] = useState([]);
 
@@ -30,11 +28,11 @@ const SearchComponent = () => {
 
     // Format start date and end date
     const startDate = searchData.startDate
-      ? moment(searchData.startDate).format('YYYY-MM-DD')
-      : '';
+      ? moment(searchData.startDate).format("YYYY-MM-DD")
+      : "";
     const endDate = searchData.endDate
-      ? moment(searchData.endDate).format('YYYY-MM-DD')
-      : '';
+      ? moment(searchData.endDate).format("YYYY-MM-DD")
+      : "";
 
     const queryString = new URLSearchParams({
       destination: searchData.destination,
@@ -42,7 +40,7 @@ const SearchComponent = () => {
       endDate,
       guests: searchData.guests,
       numRooms: searchData.numRooms,
-      localId: searchData.localId ? 'true' : '',
+      localId: searchData.localId ? "true" : "",
       moreOptions: searchData.moreOptions,
     }).toString();
 
@@ -51,32 +49,36 @@ const SearchComponent = () => {
       .then((response) => response.json())
       .then((data) => {
         setSearchResults(data);
-        console.log(data[0], "Search results")
-        dispatch(getSearchState(data))
-        navigate("/search/results")
+        console.log(data[0], "Search results");
+        dispatch(getSearchState(data));
+        navigate("/search/results");
       })
       .catch((error) => {
-        console.error('Error fetching search results:', error);
+        console.error("Error fetching search results:", error);
       });
   };
   //====== /search/results
- 
+
   const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
-    const inputValue = type === 'checkbox' ? checked : (type === 'select-one' && value === '') ? '' : value;
+    const inputValue =
+      type === "checkbox"
+        ? checked
+        : type === "select-one" && value === ""
+        ? ""
+        : value;
     setSearchData((prevState) => ({
       ...prevState,
       [name]: inputValue,
     }));
   };
-  
 
   const renderOptions = (start, end, selectedValue) => {
     const options = [];
 
     for (let i = start; i <= end; i++) {
       const value = i.toString();
-      const label = i === end ? value : value + '';
+      const label = i === end ? value : value + "";
       options.push(
         <option key={value} value={value} selected={selectedValue === value}>
           {label}
@@ -102,24 +104,24 @@ const SearchComponent = () => {
         />
 
         <div className="date-picker">
-          <div className="start-date">
-            <DatePicker
-              id="start-date"
-              name="startDate"
-              selected={searchData.startDate}
-              onChange={(date) =>
-                setSearchData((prevState) => ({
-                  ...prevState,
-                  startDate: date,
-                }))
-              }
-              required
-              className="input-startdate"
-              placeholderText="Check-in"
-              dateFormat="dd-MM-yyyy" // Set the desired date format
-              utcOffset={-1 * new Date().getTimezoneOffset()} // Set the UTC offset to the current local offset
-            />
-          </div>
+        <div className="start-date">
+        <DatePicker
+          id="start-date"
+          name="startDate"
+          selected={searchData.startDate}
+          onChange={(date) =>
+            setSearchData((prevState) => ({
+              ...prevState,
+              startDate: date,
+            }))
+          }
+          required
+          className="input-startdate"
+          placeholderText="Check-in"
+          dateFormat="dd-MM-yyyy"
+          utcOffset={-1 * new Date().getTimezoneOffset()}
+        />
+      </div>
           <div className="end-date">
             <DatePicker
               id="endDate"
@@ -139,86 +141,79 @@ const SearchComponent = () => {
             />
           </div>
           <div className="marital">
-          <select
-            id="maritalStatus"
-            name="maritalStatus"
-            value={searchData.maritalStatus}
-            onChange={handleInputChange}
-            required
-            className="input-select-marital"
-          >
-            <option value="">Marital</option>
-            <option value="single">Single</option>
-            <option value="married">Married</option>
-          </select>
+            <select
+              id="maritalStatus"
+              name="maritalStatus"
+              value={searchData.maritalStatus}
+              onChange={handleInputChange}
+             
+              className="input-select-marital"
+            >
+              <option value="">Select</option>
+              <option value="unmarried">Unmarried</option>
+              <option value="married">Married</option>
+            </select>
+          </div>
         </div>
-        </div>
-<div className='guest-room'>
-        <div className="guests">
-        <p>Guest</p>
-          <select
-            id="guests"
-            name="guests"
-            value={searchData.guests}
-            
-            onChange={handleInputChange}
-            required
-            className="input-guests"
-          >
-            {renderOptions(1, 1000, 'Guests')}
-          </select>
-          
-        </div>
+        <div className="guest-room">
+          <div className="guests">
+            <p>Guest</p>
+            <select
+              id="guests"
+              name="guests"
+              value={searchData.guests}
+              onChange={handleInputChange}
+              required
+              className="input-guests"
+            >
+              {renderOptions(1, 1000, "Guests")}
+            </select>
+          </div>
 
-        <div className="rooms">
-        <p>Room</p>
-          <select
-            id="numRooms"
-            name="numRooms"
-            value={searchData.numRooms}
-            onChange={handleInputChange}
-            required
-            className="input-rooms"
-          >
-            {renderOptions(1, 4, 'Rooms')}
-          </select>
-          
-        </div>
-        <div className="more-options">
-        <p>More</p>
-          <select
-            id="moreOptions"
-            name="moreOptions"
-            value={searchData.moreOptions}
-            onChange={handleInputChange}
-            className="input-select-more"
-          >
-            <option value="">More</option>
-            <option value="Pets Allowed">Pets Allowed</option>
-            <option value="Alcohol Allowed">Alcohol Allowed</option>
-            <option value="Bachelor Allowed">Bachelors Allowed</option>
-            <option value="Smoking Allowed">Smoking Allowed</option>
-          </select>
-        </div>
+          <div className="rooms">
+            <p>Room</p>
+            <select
+              id="numRooms"
+              name="numRooms"
+              value={searchData.numRooms}
+              onChange={handleInputChange}
+              required
+              className="input-rooms"
+            >
+              {renderOptions(1, 4, "Rooms")}
+            </select>
+          </div>
+          <div className="more-options">
+            <p>More</p>
+            <select
+              id="moreOptions"
+              name="moreOptions"
+              value={searchData.moreOptions}
+              onChange={handleInputChange}
+              className="input-select-more"
+            >
+              <option value="">More</option>
+              <option value="Pets Allowed">Pets Allowed</option>
+              <option value="Alcohol Allowed">Alcohol Allowed</option>
+              <option value="Bachelor Allowed">Bachelors Allowed</option>
+              <option value="Smoking Allowed">Smoking Allowed</option>
+            </select>
+          </div>
         </div>
 
         <div className="local">
-  <label htmlFor="localId">
-    <input
-      type="checkbox"
-      id="localId"
-      name="localId"
-      checked={searchData.localId}
-      onChange={handleInputChange}
-      className="input-checkbox"
-    />
-    Local ID
-  </label>
-</div>
-
-       
-
-        
+          <label htmlFor="localId">
+            <input
+              type="checkbox"
+              id="localId"
+              name="localId"
+              checked={searchData.localId}
+              onChange={handleInputChange}
+              className="input-checkbox"
+            />
+            Local ID
+          </label>
+        </div>
 
         <div className="btn">
           <button type="submit">Search</button>
