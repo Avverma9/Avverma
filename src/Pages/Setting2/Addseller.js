5899541a4dc7e3deafc3fce44de4132631271414
Abdul-rub <arub0419@gmail.com>
2023-08-13 11:25:02 +0530

    sourav-merged

---
+++ src/Pages/Setting2/Addseller.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Addseller() {
+  return (
+    <div className='setting-container'>
+    <div className='setting-head'>
+        <h1>Add Seller</h1>
+    </div>
+    <div className='setting-dropdown'>
+    <label>Add Seller
+    <input type='text'/>
+    </label>
+    </div>
+    <button className='sub-button'>Submit</button>
+      
+    </div>
+  )
+}
+
+export default Addseller
