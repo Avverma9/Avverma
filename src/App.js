15655a0dac5e1e0d93ecfaeab7c9ae40c592f928
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-13 22:58:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { PageContainer } from "./Component/PageContainer/PageContainer";
 import { SideNavBar } from "./Component/SideNavBar/SideNavBar";
-import { Editemd } from "./Pages/Editemd/Editemd";
+
 
 function App() {
 	return (
 		<div className='app_container'>
 			<BrowserRouter>
-			<Editemd/>
+			
 				<SideNavBar />
 				<PageContainer />
 			</BrowserRouter>
