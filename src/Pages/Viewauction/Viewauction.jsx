8c2a8840ced87648958c6584ba1e2efb4ff27cce
Sourav-source <st.sourav.xaverian@gmail.com>
2023-08-17 11:42:36 +0530

    update 6 files

--- src/Pages/Viewauction/Viewauction.jsx
+++ src/Pages/Viewauction/Viewauction.jsx
@@ -1,17 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
-import SimpleImageSlider from "react-simple-image-slider";
 import DataTable from "react-data-table-component";
 import { RiArrowLeftDoubleFill, RiArrowRightDoubleFill } from "react-icons/ri";
 import { BiEdit } from "react-icons/bi";
 import { MdDeleteOutline } from "react-icons/md";
 import { FiUpload } from "react-icons/fi";
+import ImageGallery from "react-image-gallery";
 
 import "./Viewauction.css";
 
 export const Viewauction = () => {
   const [auctionData, setAuctionData] = useState(null);
   const { id } = useParams();
+
+  const slideShowref = useRef();
+
+  // const currentImage = getCurrentIndex();
+
+  const setPrimary = () => {};
+
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
 
   const fetchData = async () => {
     try {
@@ -79,19 +100,15 @@ export const Viewauction = () => {
     <div className="addauction-preview">
       {auctionData && (
         <>
-          <div className="image-slider">
-            <div className="image-header-contains">
-              <button>Set Primary</button>
-              <BiEdit size="20" color="#000000" />
-              <MdDeleteOutline size="20" color="#ff0000" />
-            </div>
-            <div className="image-slide">
-              <RiArrowLeftDoubleFill />
-              {/* {images.map((i) => (
-            <img src={i.url} alt="i-banner" />
-          ))} */}
-              <RiArrowRightDoubleFill />
-            </div>
+          <ImageGallery
+            ref={slideShowref}
+            items={images}
+            thumbnailPosition="left"
+            lazyLoad="true"
+            autoPlay="true"
+          />
+          <div>
+            <button onClick={setPrimary}>Set Primary</button>
           </div>
           <div className="biding-details-table">
             <DataTable
