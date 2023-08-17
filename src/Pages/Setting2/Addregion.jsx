40b98b0f1a5ca366d4192abaf7b985ab236e4079
Abdul-rub <arub0419@gmail.com>
2023-08-17 15:14:27 +0530

    seller-region-category

--- src/Pages/Setting2/Addregion.jsx
+++ src/Pages/Setting2/Addregion.jsx
@@ -1,10 +1,19 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Addregion() {
   const [name, setName] = useState("");
+  const [isValid, setIsValid] = useState(true); 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    const regexPattern = /^[A-Za-z\s]+$/;
+
+    if (!regexPattern.test(name)) {
+      setIsValid(false);
+      return;
+    }
+
     try {
       const response = await fetch("http://13.48.45.18:4008/admin/region/add", {
         method: "POST",
@@ -16,15 +25,17 @@ function Addregion() {
           name: name,
         }),
       });
-      console.log(response);
+      
       if (response && response.ok === true) {
         alert("Added successfully");
         setName("");
+        setIsValid(true);
       }
     } catch (error) {
       console.error(error);
     }
   };
+
   return (
     <div className="setting-container">
       <form onSubmit={handleSubmit}>
@@ -37,10 +48,15 @@ function Addregion() {
             <input
               type="text"
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => {
+                setName(e.target.value);
+                setIsValid(true); 
+              }}
+              className={!isValid ? "invalid-input" : ""}
             />
           </label>
         </div>
+          {!isValid && <div style={{color:"red"}}>Invalid input. Only letters and spaces allowed.</div>}
         <button className="sub-button">Submit</button>
       </form>
     </div>
--- src/Pages/Setting2/Addseller.jsx
+++ src/Pages/Setting2/Addseller.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function Addseller() {
   const [name, setName] = useState("");
+  const [isValid, setIsValid] = useState(true); 
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+
+    const regexPattern = /^[A-Za-z\s]+$/;
+
+    if (!regexPattern.test(name)) {
+      setIsValid(false);
+      return;
+    }
+
     try {
       const response = await fetch("http://13.48.45.18:4008/admin/seller/add", {
         method: "POST",
@@ -16,15 +26,17 @@ function Addseller() {
           name: name,
         }),
       });
-      console.log(response);
+      
       if (response && response.ok === true) {
         alert("Added successfully");
         setName("");
+        setIsValid(true); 
       }
     } catch (error) {
       console.error(error);
     }
   };
+
   return (
     <div className="setting-container">
       <form onSubmit={handleSubmit}>
@@ -37,10 +49,15 @@ function Addseller() {
             <input
               type="text"
               value={name}
-              onChange={(e) => setName(e.target.value)}
+              onChange={(e) => {
+                setName(e.target.value);
+                setIsValid(true);
+              }}
+              className={!isValid ? "invalid-input" : ""}
             />
           </label>
         </div>
+          {!isValid && <div style={{color:"red"}}>Invalid input. Only letters and spaces allowed.</div>}
         <button className="sub-button">Submit</button>
       </form>
     </div>
