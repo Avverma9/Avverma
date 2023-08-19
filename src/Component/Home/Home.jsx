41547b05c588559c93a57fa7291ba3ae0c83122b
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-08-19 15:26:47 +0530

    update

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,31 +1,54 @@
-import React, { useEffect,useState } from 'react';
-import './Home.css'
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import './Home.css';
 
+function Home() {
+  const [images, setImages] = useState([]);
+  const [slideIndex, setSlideIndex] = useState(0);
 
-const Home = () => {
-  const[data,setData]=useState([])
-  useEffect(()=>{
-    fetch("https://hotel-backend-tge7.onrender.com/get/second/carousel")
-    .then(res=>res.json())
-    .then(data=>setData(data))
-  })
-  const location=useLocation()
-  if( location.pathname !== "/"){
-    return null
-    }
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch('https://hotel-backend-tge7.onrender.com/get/second/carousel');
+        const data = await response.json();
+        const allImages = data.map(result => result.images).flat();
+        setImages(allImages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  const goToNextSlide = () => {
+    setSlideIndex(prevIndex => (prevIndex + 1) % images.length);
+  };
+
+  const goToPreviousSlide = () => {
+    setSlideIndex(prevIndex => (prevIndex - 1 + images.length) % images.length);
+  };
+
   return (
-    <div>
-     
-      {data.map(e=>(
-        <div key={e._id}>
-          <img src={e.images[0]} alt="" />
-          <img src={e.images[1]} alt="" />
-          <img src={e.images[2]} alt="" />
+    <div className="slider-img02">
+      {images.length > 0 && (
+        <div className="slider-container02">
+          <img
+            src={images[slideIndex]}
+            alt={`Slide ${slideIndex + 1}`}
+            style={{ display: 'block' }}
+          />
+          <div className="slider-arrows02">
+            <div className="slider-arrow-left-arrow02" onClick={goToPreviousSlide}>
+              &lt;
+            </div>
+            <div className="slider-arrow-right-arrow02" onClick={goToNextSlide}>
+              &gt;
+            </div>
+          </div>
         </div>
-      ))}
+      )}
     </div>
   );
-};
+}
 
 export default Home;
