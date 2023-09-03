c5bc6a838f598b8c873bc26453777f0a882258bd
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-09-04 01:34:37 +0530

    done

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -175,24 +175,12 @@ const getCheckingBooking = async (req, res) => {
 
 //========================================================================
 const updateBooking = async (req, res) => {
-
-  try {
-    const { id } = req.params;
-    let data = req.body
-    const booking = await bookingModel.findOneAndUpdate({booking:id},data,{new: true});
-
-    if (!booking) {
-      return res.status(404).json({ success: false, message: "Booking not found" });
-    }
-
-    await booking.save();
-
-    res.status(200).json({ success: true, data: booking });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
-
+const {bookingId} = req.params
+const data = req.body
+const updatedData = await bookingModel.findOneAndUpdate({bookingId:bookingId}, {$set:data},{new:true})
+res.json(updatedData)
 };
+
 
 
 
