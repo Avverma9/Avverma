3e7df82ad1402ef37cd7835d2edf28f1ecdc14c4
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-09-04 15:59:30 +0530

    done

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -163,7 +163,7 @@ const deleteReview = async (req, res) => {
     return res.status(200).json({ message: 'Review deleted successfully' });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error' })
   }
 };
 //===========================get all  reviews ==========================================
