d18f224dd69780a3e2b299e1d2c7f4a55cd2aa94
hardik-pratap-singh <21bcs090@iiitdmj.ac.in>
2023-09-14 02:11:32 +0530

    card UI : Finalized

--- src/Component/Profile/ConfirmBooking.jsx
+++ src/Component/Profile/ConfirmBooking.jsx
@@ -88,61 +88,58 @@ export const ConfirmBooking = ({ toast }) => {
             bookingDetails.map((bookingDetails) => {
               return (<>
                 <div className="card" style={{ display: "inline-block", width: "22rem", marginLeft: "-10px", marginBottom: "12px", marginRight: "30px" }}>
-                <h5><strong>Booking ID</strong></h5>
-                
-                <h5 style={{fontWeight : "lighter"}}>{bookingDetails.bookingId}</h5>
-                <hr />
-                <h6>{bookingDetails.hotelName}</h6><br />
-                <h6>Hotel Destination</h6>
-                <h6 style={{fontWeight : "lighter"}}>{bookingDetails.destination}</h6><br />
-                <h6 style={{fontSize : "15px"}}>Landmark</h6>
+                  <h5><strong>Booking ID</strong></h5>
+
+                  <h5 style={{ fontWeight: "lighter" }}>{bookingDetails.bookingId}</h5>
+                  <hr />
+                  <h6>{bookingDetails.hotelName}</h6><br />
+                  <h6>Hotel Destination</h6>
+                  <h6 style={{ fontWeight: "lighter" }}>{bookingDetails.destination}</h6><br />
+                  <h6 style={{ fontSize: "15px" }}>Landmark</h6>
                   <img src={`${bookingDetails.images}`} className="card-img-top" alt="..." style={styles.imgConfig} />
-                 <hr />
-                 <br />
+                  <hr />
+                  <br />
 
-                 <div className="first" style={{display : "flex" , width : "full" , justifyContent : "space-between"}}>
-                  <div className="bookingdate">
-                      <h6 style={{color : "grey"}}>Booking Date</h6>
-                      <h6 style={{color : "black" , fontSize : "15px" }}>{bookingDetails.createdAt.substring(0, 10)}</h6>
+                  <div className="first" style={{ display: "flex", width: "full", justifyContent: "space-between" }}>
+                    <div className="bookingdate" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                      <h6 style={{ color: "grey" }}>Booking Date</h6>
+                      <h6 style={{ color: "black", fontSize: "15px" }}>{bookingDetails.createdAt.substring(0, 10)}</h6>
+                    </div>
+                    <div className="bookingtime" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                      <h6 style={{ color: "grey" }}>Booking Time</h6>
+                      <h6 style={{ fontSize: "15px" }}>{bookingDetails.createdAt.substring(11, 19)}</h6>
+                    </div>
                   </div>
-                  <div className="bookingtime">
-                      <h6 style={{color : "grey"}}>Booking Time</h6>
-                      <h6 style={{fontSize : "15px" }}>{bookingDetails.createdAt.substring(11, 19)}</h6>
+                  <div className="first" style={{ display: "flex", width: "full", justifyContent: "space-between" }}>
+                    <div className="bookingdate" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                      <h6 style={{ color: "grey" }}>Check-In-Time</h6>
+                      <h6 style={{ color: "black", fontSize: "15px" }}>{bookingDetails.checkInTime}</h6>
+                    </div>
+
                   </div>
-                 </div>
-                 <div className="first" style={{display : "flex" , width : "full" , justifyContent : "space-between"}}>
-                  <div className="bookingdate">
-                      <h6 style={{color : "grey"}}>Check-In-Time</h6>
-                      <h6 style={{color : "black" , fontSize : "15px" }}>{bookingDetails.checkInTime}</h6>
-                  </div>
-                  
-                 </div>
-                 <div className="bookingtime">
-                      <h6 style={{color : "grey"}}>Check-Out-Time</h6>
-                      <h6 style={{fontSize : "15px" }}>{bookingDetails.checkOutTime}</h6>
+                  <div className="bookingtime" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                    <h6 style={{ color: "grey" }}>Check-Out-Time</h6>
+                    <h6 style={{ fontSize: "15px" }}>{bookingDetails.checkOutTime}</h6>
                   </div>
 
-                  <div className="first" style={{display : "flex" , width : "full" , justifyContent : "space-between"}}>
-                  <div className="bookingdate">
-                      <h6 style={{color : "grey"}}>Status</h6>
-                      <h6 style={{color : "black" , fontSize : "15px" }}>{bookingDetails.bookingStatus}</h6>
-                  </div>
-                  
-                  </div>
-                  <div className="bookingtime">
-                      <h6 style={{color : "grey"}}>Guests</h6>
-                      <h6 style={{fontSize : "15px" }}>{bookingDetails.guests}</h6>
-                  </div>
-                  
-                  
+                  <div className="first" style={{ display: "flex", width: "full", justifyContent: "space-between" }}>
+                    <div className="bookingdate" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                      <h6 style={{ color: "grey" }}>Status</h6>
+                      <h6 style={{ color: "black", fontSize: "15px" }}>{bookingDetails.bookingStatus}</h6>
+                    </div>
 
-                
+                  </div>
+                  <div className="bookingtime" style={{ borderLeft: "2px solid grey", borderRadius: "12px", paddingLeft: "10px", marginBottom: "10px" }}>
+                    <h6 style={{ color: "grey" }}>Guests</h6>
+                    <h6 style={{ fontSize: "15px" }}>{bookingDetails.guests}</h6>
+                  </div>
+
                   <center>
                     <button type="button" className="btn btn-primary" style={{ fontSize: "14px", padding: "5px 5px" }} data-bs-toggle="modal" data-bs-target="#exampleModal">
                       View Details
                     </button></center>
 
-                  {/* <!-- Modal --> */}
+                  {/* <!-- Modal Starts From Here--> */}
                   <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                       <div className="modal-content">
@@ -150,8 +147,8 @@ export const ConfirmBooking = ({ toast }) => {
                           <h1 className="modal-title fs-5" id="exampleModalLabel">Booking Details</h1>
                           <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div className="modal-body">  
-                          
+                        <div className="modal-body">
+
                           <img src={`${bookingDetails.images}`} className="card-img-top" alt="..." style={styles.imgConfig} />
                           <table className="mytable">
                             <tr>
@@ -192,7 +189,7 @@ export const ConfirmBooking = ({ toast }) => {
                               <td>{bookingDetails.guests}</td>
                             </tr>
 
-                            {/* inko pop up me dikhana hai */}
+
                             <tr >
                               <td><strong>Food Items</strong></td>
                               <td>{bookingDetails.foodItems.length && bookingDetails.foodItems.map((e) => { return (<><ul><li>{e.name}</li></ul></>) })}</td>
@@ -214,65 +211,18 @@ export const ConfirmBooking = ({ toast }) => {
                         </div>
                         <div className="modal-footer">
                           <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">OK</button>
-                          {/* <button type="button"  className="btn btn-primary" onClick={closeClick}>OK</button> */}
                         </div>
                       </div>
                     </div>
                   </div>
+
+                  {/* //Modal Ends Here */}
 
 
                 </div>
               </>)
             })
           }
-
-          {/* <div style={styles.tableContainer}>
-            <table style={styles.table}>
-              <thead>
-                <tr>
-                  <th style={styles.cell}>Sr</th>
-                  <th style={styles.cell}>Image</th>
-                  <th style={styles.cell}>Hotel Name</th>
-                  <th style={styles.cell}>Booking ID</th>
-                  <th style={styles.cell}>Destination</th>
-                  <th style={styles.cell}>Check In</th>
-                  <th style={styles.cell}>Check Out</th>
-                  <th style={styles.cell}>Price</th>
-                  <th style={styles.cell}>Status</th>
-                </tr>
-              </thead>
-              <tbody>
-                {bookingDetails
-                  .filter((booking) => booking.bookingStatus === "success")
-                  .map((booking,index) => (
-                    <tr key={booking.bookingId}>
-                      <td>{index +1}</td>
-                      <td style={styles.cell}>
-              <div style={styles.circularImage}>
-                <img
-                  className="hotelimage"
-                  src={booking.hotelimage}
-                  alt="Hotel"
-                  style={styles.circularImageImg}
-                />
-              </div>
-            </td>
-                      <td style={styles.cell}>{booking.hotel.hotelName}</td>
-                      <td style={styles.cell}>{booking.bookingId}</td>
-                      <td style={styles.cell}>
-                        {booking.destination ? booking.destination : "No data"}
-                      </td>
-                      <td style={styles.cell}>{formatDate(booking.checkInDate)}</td>
-                      <td style={styles.cell}>{formatDate(booking.checkOutDate)}</td>
-                      <td style={styles.cell}>{booking.price}</td>
-                      <td style={{ ...styles.cell, color: "green" }}>
-                        {booking.bookingStatus}
-                      </td>
-                    </tr>
-                  ))}
-              </tbody>
-            </table>
-          </div>; */}
         </>
       ) : (
         <p>No Data Found...</p>
