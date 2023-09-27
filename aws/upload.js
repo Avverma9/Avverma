a4fc76496787dc93cb67b4e94f97d7b233a2811f
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-09-28 02:03:26 +0530

    done

--- aws/upload.js
+++ aws/upload.js
@@ -1,9 +1,9 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
-const AWS_BUCKET_NAME = "avvermabucket";
-const AWS_ACCESS_KEY_ID = "AKIARN4LFJPVCZH7XWS7";
-const AWS_SECRET_ACCESS_KEY = "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";
+const AWS_BUCKET_NAME = "avverma";
+const AWS_ACCESS_KEY_ID = "AKIARRSTFGSV74HCL27W";
+const AWS_SECRET_ACCESS_KEY = "pa9q2g1hZKruk0H33+6eRZ7yiGVx/jycGsdpP+4Q";
 const AWS_REGION = "ap-south-1"; // Update this to the appropriate region for your S3 bucket
 const s3 = new S3Client({
   region: AWS_REGION,
