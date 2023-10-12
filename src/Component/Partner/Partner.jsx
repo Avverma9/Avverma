3fbddcbc30537d948f2158a549bc081a2841184f
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2023-10-12 12:43:21 +0530

    latest one

--- src/Component/Partner/Partner.jsx
+++ src/Component/Partner/Partner.jsx
@@ -721,7 +721,7 @@ const Partner = () => {
               </div>
 
               <div className="street">
-                <label htmlFor="street">Street:</label>
+                <label htmlFor="street">Address:</label>
                 <input
                   type="text"
                   id="street"
