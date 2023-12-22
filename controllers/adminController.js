5f3771e68ea881f249d11d6c8ac06f6eba40f345
Avverma9 <av95766@gmail.com>
2023-12-22 11:25:55 +0530

    done

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,14 +1,25 @@
 const admin = require("../models/adminModel")
 const register = async(req,res)=>{
-    const {email,password}=req.body
+    const {name,email,password}=req.body
     const images = req.files.map((file)=>file.location)
-    const createAuser= await admin.create({images,email,password})
+    const createAuser= await admin.create({images,name,email,password})
     res.json(createAuser)
 }
 const signIn= async function(req,res){
     const {email,password} = req.body
     const findData = await admin.findOne({email:email,password:password})
-    res.json(findData)
+
+    if (!findData) {
+        res.status(400).json({ message: "Login failed" });
+      } else {
+        res.status(200).json({
+          message: "Logged in as",
+          loggedUserImage: findData.images,
+          loggedUserId: findData._id,
+          loggedUserName: findData.name,
+          loggedUserEmail: findData.email,
+        });
+      }
 }
 
 module.exports={register,signIn}
--- models/adminModel.js
+++ models/adminModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const adminSchema = new mongoose.Schema({
     images:[String],
+    name:String,
     email:String,
     password:String
 })
