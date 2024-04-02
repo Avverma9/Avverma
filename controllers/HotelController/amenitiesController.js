29f3698f391f6daa51ef3d0725a6233200d4f146
Avverma9 <av95766@gmail.com>
2024-04-03 02:13:39 +0530

    done

--- controllers/HotelController/amenitiesController.js
+++ controllers/HotelController/amenitiesController.js
@@ -1,18 +1,16 @@
-
 const hotels = require("../../models/Hotel/hotelModel");
 const foods = require("../../models/Hotel/foodsModel");
 const amenities = require("../../models/Hotel/amenitiesModel");
 const policies = require("../../models/Hotel/policyModel");
 
-const { v4: uuidv4 } = require('uuid');
-exports.createAmenity = async(req,res)=>{
+const { v4: uuidv4 } = require("uuid");
+exports.createAmenity = async (req, res) => {
   try {
-    const { hotelId , ...amenities} = req.body;
-    const amenityId = uuidv4().substr(0, 8); 
+    const { hotelId, ...amenities } = req.body;
+
     const createdAmenity = {
-      amenityId,
       hotelId,
-     ...amenities
+      ...amenities,
     };
     await hotels.findOneAndUpdate(
       { hotelId },
@@ -24,33 +22,31 @@ exports.createAmenity = async(req,res)=>{
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
-exports.getHotelByAmenities = async (req, res) => {
-    try {
-        const hotelData = await hotelModel.findOne();
-        
-        if (!hotelData) {
-          return res.status(404).json({ message: 'No hotel data found' });
-        }
-        const hotelId = hotelData.hotelId;
-        const foodsData = await foods.findOne({ hotelId });
-        const amenitiesData = await amenities.findOne({ hotelId });
-        const policiesData = await policies.findOne({ hotelId });
-        const roomData = await roomModel.findOne({ hotelId });
-    
-        // Return the data
-        res.json({
-          hotel: hotelData,
-          foods: foodsData,
-          amenities: amenitiesData,
-          policies: policiesData,
-          room: roomData,
-        });
-      } catch (error) {
-        // Handle errors
-        console.error(error);
-        res.status(500).json({ message: 'Internal Server Error' });
-      }
 };
+exports.getHotelByAmenities = async (req, res) => {
+  try {
+    const hotelData = await hotelModel.findOne();
 
-  
+    if (!hotelData) {
+      return res.status(404).json({ message: "No hotel data found" });
+    }
+    const hotelId = hotelData.hotelId;
+    const foodsData = await foods.findOne({ hotelId });
+    const amenitiesData = await amenities.findOne({ hotelId });
+    const policiesData = await policies.findOne({ hotelId });
+    const roomData = await roomModel.findOne({ hotelId });
+
+    // Return the data
+    res.json({
+      hotel: hotelData,
+      foods: foodsData,
+      amenities: amenitiesData,
+      policies: policiesData,
+      room: roomData,
+    });
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
