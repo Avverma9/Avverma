b35ba1c8da65012f3cce17f29b638f13778f8454
Ankit Kumar Verma <110284078+Avverma9@users.noreply.github.com>
2024-04-16 06:06:18 +0000

    done

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const cronJob = () => {
 cron.schedule("0 0 * * *", cronJob);
 
 // Allow requests only from specific origins
-const allowedOrigins = ["https://roomsstay.vercel.app", "http://example2.com"];
+const allowedOrigins = ["https://hotel-backend-tge7.onrender.com","http://localhost:3000","http://localhost:3001"];
 app.use(cors({
   origin: function(origin, callback) {
     // Check if the origin is allowed or if it's a request from the same origin (null)
