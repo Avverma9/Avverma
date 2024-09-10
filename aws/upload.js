5408c6044bc62a68f45021c84627b7954a23a9ae
Avverma95 <ankit.verma@chplgroup.org>
2024-09-10 17:27:07 +0530

    aws account changed

--- aws/upload.js
+++ aws/upload.js
@@ -1,10 +1,10 @@
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multerS3 = require("multer-s3");
 const multer = require("multer");
-const AWS_BUCKET_NAME = "avverma";
-const AWS_ACCESS_KEY_ID = "AKIARRSTFGSV74HCL27W";
-const AWS_SECRET_ACCESS_KEY = "pa9q2g1hZKruk0H33+6eRZ7yiGVx/jycGsdpP+4Q";
-const AWS_REGION = "ap-south-1"; // Update this to the appropriate region for your S3 bucket
+const AWS_BUCKET_NAME = "happy-stay-hopspitiality";
+const AWS_ACCESS_KEY_ID = "AKIAX3DNHWL2NL4YPLUL";
+const AWS_SECRET_ACCESS_KEY = "555f4HBPMcslvv1drMqIx0UkrmM0MJm844jHUGBk";
+const AWS_REGION = "eu-north-1"; // Update this to the appropriate region for your S3 bucket
 const s3 = new S3Client({
   region: AWS_REGION,
   credentials: {
