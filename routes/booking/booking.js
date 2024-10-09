0bf8646d660372beed06a99770aec79e00e4eb46
avverma9 <av95766@gmail.com>
2024-10-09 13:19:13 +0530

    filter booking me date add kiya

--- routes/booking/booking.js
+++ routes/booking/booking.js
@@ -4,13 +4,12 @@ const { upload } = require('../../aws/upload');
 const bookingController = require('../../controllers/booking/booking');
 const auth = require('../../authentication/auth');
 
-router.post('/booking/:userId/:hotelId', upload, bookingController.createBooking);
+router.post('/booking/:userId/:hotelId', upload, bookingController.createBooking); // on site
 
-router.put('/updatebooking/:bookingId', auth, bookingController.updateBooking);
+router.put('/updatebooking/:bookingId', auth, bookingController.updateBooking); //on panel
 
 router.get('/get/all/users-filtered/booking/by', bookingController.getAllFilterBookings); // using on main site
 router.get('/get/all/filtered/booking/by/query', bookingController.getAllFilterBookingsByQuery); //using on dashboard
-router.get('/get-all/bookings-details', auth, bookingController.getAll);
 router.get('/get-all/bookings-count', bookingController.getBookingCounts);
 router.get('/get-all/sell-count', bookingController.getTotalSell);
 
