2b6174ab0bff6a729f978964b5c9b87296b94b61
avverma9 <av95766@gmail.com>
2024-11-26 19:42:14 +0530

    ui changes

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,6 @@
 // const baseURL = "https://hotel-backend-tge7.onrender.com"
-const baseURL = "http://localhost:5000";
+// const baseURL = "http://localhost:5000";
+const baseURL = "https://dfbd-122-170-112-50.ngrok-free.app";
+
+
 export default baseURL;
