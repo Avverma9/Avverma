211f2e597a229b17a74ce809c103b82e1ddfe55f
Avverma9 <av95766@gmail.com>
2025-04-01 11:43:44 +0530

    fix: update CORS configuration to allow requests from any origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: ['http://localhost:3030', 'https://roomsstay.vercel.app'], // Your client origins
+        origin: '*', // Your client origins
         methods: ['GET', 'POST'],
         credentials: true,
     },
