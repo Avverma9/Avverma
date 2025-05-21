c4197b30c4b6dfae0a2ef79ecc4af9ddb9509d54
Avverma9 <av95766@gmail.com>
2025-05-21 20:27:58 +0530

    done

--- routes/GST/gst.js
+++ routes/GST/gst.js
@@ -1,9 +1,9 @@
 const express = require('express'); 
-const { createGST, updateGST } = require('../../controllers/GST/gst');
+const { createGST, updateGST, getGST, getAllGST } = require('../../controllers/GST/gst');
 const router = express.Router();
 
 router.post('/create-gst', createGST); // Create a new GST entry
 router.patch('/update-gst', updateGST); // Update the latest GST entry
-
-
+router.get('/get-single-gst', getGST); // Get a single GST entry
+router.get('/get-all-gst', getAllGST); // Get all GST entries
 module.exports = router
