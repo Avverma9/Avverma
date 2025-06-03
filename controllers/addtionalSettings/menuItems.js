3112e11db3d709f52a445ba48bd74107937661b0
Avverma9 <av95766@gmail.com>
2025-06-03 16:15:52 +0530

    changes

---
+++ controllers/addtionalSettings/menuItems.js
@@ -0,0 +1,35 @@
+const MenuItem = require("../../models/additionalSettings/menuItem");
+
+exports.addMenu = async (req, res) => {
+    try {
+        const menuItem = new MenuItem(req.body);
+        await menuItem.save();
+        res.status(201).json({ message: 'Menu item added successfully', menuItem });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+// 🔹 Add bulk menu items
+exports.addMenuBulk = async (req, res) => {
+    try {
+        const data = req.body; // should be an array
+        if (!Array.isArray(data)) {
+            return res.status(400).json({ error: 'Data should be an array' });
+        }
+
+        const insertedItems = await MenuItem.insertMany(data);
+        res.status(201).json( insertedItems);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+exports.getAllMenuItems = async (req, res) => {
+    try {
+        const menuItems = await MenuItem.find();
+        res.status(200).json(menuItems);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
--- routes/additional/additional.js
+++ routes/additional/additional.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { addTravelAmenities, getTravelAmenities } = require('../../controllers/addtionalSettings/travelAmenities');
+const { addMenuBulk, getAllMenuItems } = require('../../controllers/addtionalSettings/menuItems');
 const router = express.Router();
 
+//==================================amenities=============================
 router.post ("/add/travel-amenities",addTravelAmenities)
 router.get("/get/travel-amenities",getTravelAmenities)
+//=====================================MenuItems==============================
+router.post('/bulk-add-menuItems',addMenuBulk)
+router.get("/get-menu-items",getAllMenuItems)
+
+
 
 module.exports = router
+
+
+// 
