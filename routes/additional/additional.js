d04694171c47318f84e456a84fc3d15e36487973
Avverma9 <av95766@gmail.com>
2025-06-03 16:47:10 +0530

    done

--- routes/additional/additional.js
+++ routes/additional/additional.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { addTravelAmenities, getTravelAmenities } = require('../../controllers/addtionalSettings/travelAmenities');
 const { addMenuBulk, getAllMenuItems } = require('../../controllers/addtionalSettings/menuItems');
 const { addBed, addBedBulk, getBed } = require('../../controllers/addtionalSettings/bedList');
+const { addRoom, getRooms, addRoomBulk } = require('../../controllers/addtionalSettings/roomList');
 const router = express.Router();
 
 //==================================amenities=============================
@@ -15,6 +16,12 @@ router.get("/get-menu-items",getAllMenuItems)
 router.post("/add-bed",addBed)
 router.get("/get-bed",getBed)
 router.post("/add-bed-bulk",addBedBulk)
+
+//=====================================Room List ===========================
+
+router.post("/add-room",addRoom)
+router.get("/get-room",getRooms)
+router.post("/add-room-bulk",addRoomBulk)
 
 
 module.exports = router
