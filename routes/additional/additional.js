0db603c03fe72516955e02a95e39e23818fda109
Avverma9 <av95766@gmail.com>
2025-06-03 17:05:38 +0530

    done

--- routes/additional/additional.js
+++ routes/additional/additional.js
@@ -3,27 +3,27 @@ const { addTravelAmenities, getTravelAmenities } = require('../../controllers/ad
 const { addMenuBulk, getAllMenuItems } = require('../../controllers/addtionalSettings/menuItems');
 const { addBed, addBedBulk, getBed } = require('../../controllers/addtionalSettings/bedList');
 const { addRoom, getRooms, addRoomBulk } = require('../../controllers/addtionalSettings/roomList');
+const { addAmenities, addBulkAmenities, getAmenities } = require('../../controllers/addtionalSettings/hotelAmenities');
 const router = express.Router();
 
 //==================================amenities=============================
-router.post ("/add/travel-amenities",addTravelAmenities)
-router.get("/get/travel-amenities",getTravelAmenities)
+router.post("/add/travel-amenities", addTravelAmenities)
+router.get("/get/travel-amenities", getTravelAmenities)
 //=====================================MenuItems==============================
-router.post('/bulk-add-menuItems',addMenuBulk)
-router.get("/get-menu-items",getAllMenuItems)
-
+router.post('/bulk-add-menuItems', addMenuBulk)
+router.get("/get-menu-items", getAllMenuItems)
 //========================================Bed list=============================
-router.post("/add-bed",addBed)
-router.get("/get-bed",getBed)
-router.post("/add-bed-bulk",addBedBulk)
-
+router.post("/add-bed", addBed)
+router.get("/get-bed", getBed)
+router.post("/add-bed-bulk", addBedBulk)
 //=====================================Room List ===========================
-
-router.post("/add-room",addRoom)
-router.get("/get-room",getRooms)
-router.post("/add-room-bulk",addRoomBulk)
-
-
+router.post("/add-room", addRoom)
+router.get("/get-room", getRooms)
+router.post("/add-room-bulk", addRoomBulk)
+//==================================Hotel Amenities===============================
+router.post("/add-amenities", addAmenities)
+router.get("/get-amenities", getAmenities)
+router.post("/add-amenities-bulk", addBulkAmenities)
 module.exports = router
 
 
