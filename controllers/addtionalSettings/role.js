76110451fe48c71efb29e9d89492c1a1892141a5
Avverma9 <av95766@gmail.com>
2025-06-08 12:07:45 +0530

    done

--- controllers/addtionalSettings/role.js
+++ controllers/addtionalSettings/role.js
@@ -19,7 +19,7 @@ exports.addRole = async (req, res) => {
 exports.getRole = async (req, res) => {
   try {
     const roles = await Role.find();
-    return res.status(200).json({ roles });
+    return res.status(200).json( roles );
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error", error });
   }
