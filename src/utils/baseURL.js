846a2826d3527743d10ff0467b5137aaecd6542e
Ankit Kumar Verma <av95766@gmail.com>
2025-07-09 15:08:25 +0530

    Update baseURL.js

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,4 @@
- const baseURL = "https://hotel-backend-tge7.onrender.com"
+ const baseURL = "https://hotel-backend-ntbh.onrender.com"
 // const baseURL = 'http://localhost:5000';
 
 export default baseURL;
