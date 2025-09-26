5eb89b2b305d563b13cbe49454c3b6636a895388
avverma9 <av95766@gmail.com>
2025-09-26 15:23:24 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ import AmenitiesPage from "./pages/partner/hotel-amenities";
 import PartnerFoods from "./pages/partner/hotel-foods";
 import PartnerRooms from "./pages/partner/hotel-rooms";
 import TourBookPage from "./pages/tour/tour-booking";
-import ProfileSidebar from "./components/profile/ProfileSidebar";
 import CouponPage from "./components/profile/Coupons";
+import { Sidebar } from "./components/profile";
 
 export default function App() {
   return (
@@ -39,7 +39,7 @@ export default function App() {
           <Header />
           <BookNow />
           <HeaderTravel />
-          <ProfileSidebar />
+          <Sidebar />
           <Hotel />
           <SearchForm />
           <Banner />
