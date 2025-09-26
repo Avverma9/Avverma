db93ca54e25fbe38e0913307bea0fd549376dcde
avverma9 <av95766@gmail.com>
2025-09-26 16:09:05 +0530

    k

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,10 +1,10 @@
 
 
 export { default as Sidebar } from './ProfileSidebar.jsx'
-export { default as CouponPage } from "./coupons";
-export { default as Profile } from "./profile";
-export { default as Reviews } from "./my-reviews";
-export { default as Bookings } from "./my-bookings";
-export { default as UpdateProfile } from "./update-profile";
-export { default as Complaints } from './Complaints'
-export { default as TourBooking } from './tour-booking'
+export { default as CouponPage } from "./coupons.jsx";
+export { default as Profile } from "./profile.jsx";
+export { default as Reviews } from "./my-reviews.jsx";
+export { default as Bookings } from "./my-bookings.jsx";
+export { default as UpdateProfile } from "./update-profile.jsx";
+export { default as Complaints } from './Complaints.jsx'
+export { default as TourBooking } from './tour-booking.jsx'
